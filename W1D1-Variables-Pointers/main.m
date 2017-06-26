47851a61ddf71462e9539ac22564c127edb558da
steve thompson <steven.thompson.ca@gmail.com>
2017-06-26 15:29:13 -0400

    Initial Commit

---
+++ W1D1-Variables-Pointers/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  W1D1-Variables-Pointers
+//
+//  Created by steve on 2017-06-26.
+//  Copyright © 2017 steve. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
